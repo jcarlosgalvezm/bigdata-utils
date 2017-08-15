b6b22f8110445eab28b2e53c2af1e0cfa80849bf
Carlos Gálvez <jcarlosgalvezm@gmail.com>
2017-08-15 12:23:16 +0200

    Add files via upload

---
+++ prepair_cluster.sh
@@ -0,0 +1,56 @@
+#Prepair a cluster
+
+host_name=$1
+
+cm='192.168.1.120'
+nodo1='192.168.1.121'
+nodo2='192.168.1.122'
+nodo3='192.168.1.123'
+nodo4='192.168.1.124'
+
+case $host_name in
+        "cm") ip_addr=$cm;;
+        "nodo1") ip_addr=$nodo1;;
+        "nodo2") ip_addr=$nodo2;;
+        "nodo3") ip_addr=$nodo3;;
+        "nodo4") ip_addr=$nodo4;;
+        *) echo 'ERROR: Unknown hostname'
+           exit 0;;
+esac
+
+echo '# Generating the ssh directory...'
+mkdir ~/.ssh/
+
+echo '# Updating the system packages...'
+yum update
+
+packagelist='net-tool epel-release'
+yum install -y $packagelist
+
+if [ $host_name == "cm" ]; then
+        groupslist='"X Window System" "Xfce"'
+        yum groupinstall -y $groupslist
+fi
+
+echo '# Setting the hostname...'
+hostname $host_name
+echo $host_name > /etc/hostname
+
+echo '# Setting the namespaces...'
+echo -e "${cm}"   '             cm                              cm\n'\
+"${nodo1}"'             nodo1.hdp.hadoop                nodo1\n'\
+"${nodo2}"'             nodo2.hdp.hadoop                nodo2\n'\
+"${nodo3}"'             nodo3.hdp.hadoop                nodo3\n'\
+"${nodo4}"'             nodo4.hdp.hadoop                nodo4\n' > /etc/hosts
+
+echo '# Setting ip static...'
+sed -i -e 's/BOOTPROTO="dhcp"/BOOTPROTO="static"/g' /etc/sysconfig/network-scripts/ifcfg-enp0s3
+
+echo '# Setting the ip address...'
+echo -e 'IPADDR='"${ip_addr}"'\n'\
+'NETMASK="255.255.255.0"\n'\
+'GATEWAY="192.168.1.1"\n'\
+'DNS1="8.8.8.8"' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+
+echo '# Restarting the network interface...'
+systemctl restart network
